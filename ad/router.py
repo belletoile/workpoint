9a4f9ed34338c62d3c87bbace7725a3af78b6fe4
Evgeniia Kordiukova <kordeugenie@yandex.ru>
2023-11-03 16:22:11 +0500

    ad get_all

--- ad/router.py
+++ ad/router.py
@@ -15,8 +15,13 @@ router = APIRouter(
 )
 
 
+@router.get("/all_ad")
+def get_all_advertisement(session: Session = Depends(get_db)):
+    return session.query(Ad).all()
+
+
 @router.post("/upload_ad", response_model=AdBaseSchema)
-def upload_ad(payload: AdBaseSchema = Body(), file: UploadFile = File(...), session: Session = Depends(get_db)):
+def upload_advertisement(payload: AdBaseSchema = Body(), file: UploadFile = File(...), session: Session = Depends(get_db)):
     payload.photo = save_file_ad(file)
     stmt = session.query(Place.id).filter_by(name=payload.name, address=payload.address).first()
     payload.id_place = stmt.id
